48730ebddeb43071221217354a5c977a83b2c6d3
Abstract02 <adarsh526222@gmail.com>
2022-07-18 13:06:48 +0530

    added all major functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Task from './Components/Task';
 
 function App() {
+
+  const [task, settask] = useState()
+  const [data,setdata] = useState([])
+
+  const handleChange = (e) => {
+    settask(
+      e.target.value
+    )
+  }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/todos")
+      .then((res)=>{
+        const data = res.data;
+        setdata(data);
+      })
+      .catch((err)=>{console.log(err)})
+  }, [])
+  
+  function submit() {
+    
+    const todoTask = {
+      title: task,
+    }
+    console.log(task.length)
+    if (task.length > 0) {
+      axios.post("http://localhost:4000/todos", todoTask)
+        .then(() => {
+          console.log("api post sucess")
+          settask("");
+          window.location.href = "/";
+        }).catch(err => {
+          console.log(`error is ${err}`)
+        })
+    }
+    else {
+      alert("Give a title name")
+    }
+
+  }
+
   return (
     <div className="app">
       <div className='todo_list'>
@@ -13,10 +57,10 @@ function App() {
         </div>
         <div className='create_task'>
           <div className='task_title'>
-            <input type="text" placeholder='Enter the task here' ></input>
+            <input type="text" value={task} onChange={handleChange} placeholder='Enter the task here' name="title"></input>
           </div>
           <div className='submit_task'>
-          <button type='submit'>Submit</button>
+            <button type='submit' onClick={submit}>Submit</button>
           </div>
         </div>
       </div>
@@ -25,7 +69,17 @@ function App() {
           <p>Added task in to-do list</p>
         </div>
         <div className='tasksContainer'>
-            <Task />
+          {
+            data.map((taskdata,i) => {
+              return(
+                <Task
+                id = {taskdata._id}
+                title = {taskdata.title}
+                completed = {taskdata.completed}
+                /> 
+              )
+            })
+          }          
         </div>
       </div>
     </div>
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,20 +1,66 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import './Task.css'
 
-function Task() {
+function Task({ id, title, completed }) {
+
+  useEffect(() => {
+    if (completed === true) {
+      document.getElementById("box").style.background = '#202020'
+      document.getElementById("complete").style.background = '#202020'
+    }
+  }, [])
+
+
+  const Delete = () => {
+    axios
+      .delete(`http://localhost:4000/todos/${id}`)
+      .then(() => {
+        console.log("task deleted")
+        window.location.href = '/';
+      })
+      .catch(err => {
+        console.log(`error is ${err}`)
+      })
+  }
+
+  const taskStatus = () => {
+    if (completed === false) {
+      const task = {
+        "completed": true
+      }
+      console.log(task)
+      axios
+        .patch(`http://localhost:4000/todos/${id}`, task)
+        .then(() => {
+          console.log("task updated")
+
+          window.location.href = "/";
+        })
+        .catch(err => {
+          console.log(`error is ${err}`)
+        })
+    }
+    else {
+      console.log("Already Completed")
+    }
+  }
+
   return (
-    <div className='taskBox'>
-        <div className='taskBoxUpper'>
-        <h1>Task1</h1>
+
+    <div id="box" className='taskBox'>
+      <div className='taskBoxUpper'>
+        <p>{title}</p>
+      </div>
+      <div className='dividingLine'></div>
+      <div className='taskBoxLower'>
+        <div className='markCompleted'>
+          <button id="complete" type='sunmit' onClick={taskStatus}>Mark as completed</button>
         </div>
-        <div className='taskBoxLower'>
-            <div>
-            <button>Mark as completed</button>
-            </div>
-            <div>
-            <button>Delete</button>
-            </div>
+        <div className='deleteTask'>
+          <button type='submit' onClick={Delete}>Delete</button>
         </div>
+      </div>
     </div>
   )
 }
